d6b0da67df778de76882e51482ce369713cd4491
oldestcrab <oledestcrab@gmail.com>
2019-04-10 13:47:07 +0800

    itcast完成

--- by_scrapy/pipelines.py
+++ by_scrapy/pipelines.py
@@ -5,7 +5,17 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
 
-class ByScrapyPipeline(object):
+class ItcastPipeline(object):
+    def __init__(self):
+        self.file = open('./teachers.json', 'w', encoding='utf-8')
+    
     def process_item(self, item, spider):
+        content = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        self.file.write(content)
+
         return item
+
+    def close_spider(self, spider):
+        self.file.close()
--- by_scrapy/spiders/itcast.py
+++ by_scrapy/spiders/itcast.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from by_scrapy.items import ItcastItem
 
 class ItcastSpider(scrapy.Spider):
     name = 'itcast'
     allowed_domains = ['itcast.cn']
-    start_urls = ['http://www.itcast.cn/channel/teacher.shtml#ajavaee']
+    start_urls = ['http://www.itcast.cn/channel/teacher.shtml#apython']
+
     def parse(self, response):
-        with open('./3.html', 'wb') as f:
-            f.write(response.body)
+        
+        for each in response.xpath('//div[@class="li_txt"]'):
+            # 实例化对象
+            item = ItcastItem()
+
+            item['name'] = each.xpath('h3/text()').extract()[0].strip()
+            item['level'] = each.xpath('h4/text()').extract()[0].strip()
+            item['info'] = each.xpath('p/text()').extract()[0].strip()
+
+            yield item
